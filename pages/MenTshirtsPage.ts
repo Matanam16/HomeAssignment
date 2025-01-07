15053bcd64c4ceeefb8bd1e233adbc83d871ec16
Matanam16 <118213575+Matanam16@users.noreply.github.com>
2025-01-07 18:05:52 +0200

    Chnging add item

--- pages/MenTshirtsPage.ts
+++ pages/MenTshirtsPage.ts
@@ -12,14 +12,29 @@ export default class MenTshirtsPage{
 
         
     }
+    
+    // Add directly from the sub category grid
 
-     public async addToCartByProductName(productName: string)  {
-        const productWrapper = this.page.locator('.single-products', { hasText: productName });
+    //  public async addToCartByProductName(productName: string)  {
+    //     const productWrapper = this.page.locator('.single-products', { hasText: productName });
+    //     if (await productWrapper.count() === 0) {
+    //         throw new Error(`Product with name "${productName}" not found.`);
+    //     }
+    
+    //     const addToCartButton = productWrapper.locator(this.buttonLocatorWithinItem).first();
+    //     await addToCartButton.click();
+    
+    // }
+
+    // View item information
+
+    public async viewItemInfoByProductName(productName: string)  {
+        const productWrapper = this.page.locator('[class="product-image-wrapper"]', { hasText: productName });
         if (await productWrapper.count() === 0) {
             throw new Error(`Product with name "${productName}" not found.`);
         }
     
-        const addToCartButton = productWrapper.locator(this.buttonLocatorWithinItem).first();
+        const addToCartButton = productWrapper.locator('[class="nav nav-pills nav-justified"]')
         await addToCartButton.click();
     
     }
